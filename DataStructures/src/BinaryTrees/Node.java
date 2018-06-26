9b13f93d02fd9426cec8c9a0a2cdbc0134b7a055
Akhilesh.Prathuri <akhilesh.prathuri@servicenow.com>
2018-06-26 15:32:46 +0530

    Binary Trees Insert and Find Implementation

---
+++ DataStructures/src/BinaryTrees/Node.java
@@ -0,0 +1,14 @@
+package BinaryTrees;
+
+public class Node {
+   double fData;
+   int iData;
+   
+   Node leftNode;
+   Node rightNode;
+   
+   public Node(int iData, double fData){
+	   this.iData = iData;
+	   this.fData = fData;
+   }
+}
